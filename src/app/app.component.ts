77b14b7cd4e326ed52c28694acf99ed51a274032
Thien <bananakind123@gmail.com>
2022-02-10 14:44:55 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,18 @@ import { CdkDragDrop } from '@angular/cdk/drag-drop';
 })
 export class AppComponent {
   title = 'angularDragGrid';
-  items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  items:Array<any> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   drop(event: CdkDragDrop<any>) {
     this.items[event.previousContainer.data.index] = event.container.data.item;
     this.items[event.container.data.index] = event.previousContainer.data.item;
     this.test(event);
   }
-  test(event:CdkDragDrop<any>) {
-    console.log(event.previousContainer.data.index);
-    console.log(event.container.data.index);
+  test(event: CdkDragDrop<any>) {
+    console.log("previous index:",event.previousContainer.data.index);
+    console.log("current index:",event.container.data.index);
+  }
+  add() {
+    this.items.push(this.items.length);
   }
 
 }
