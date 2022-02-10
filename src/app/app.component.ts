d8b50613ab17601c78f988843bb2a6381669ddf8
Thien <bananakind123@gmail.com>
2022-02-10 14:38:53 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularDragGrid';
+  items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  drop(event: CdkDragDrop<any>) {
+    this.items[event.previousContainer.data.index] = event.container.data.item;
+    this.items[event.container.data.index] = event.previousContainer.data.item;
+    this.test(event);
+  }
+  test(event:CdkDragDrop<any>) {
+    console.log(event.previousContainer.data.index);
+    console.log(event.container.data.index);
+  }
+
 }
